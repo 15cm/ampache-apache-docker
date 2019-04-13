6461777286938cd82f908f6450ad02ad875d7742
Sinkerine <git@15cm.net>
2019-04-13 01:50:29 -0400

    Bug fix: prevent inserting duplicated records of the same track
    The bug is due to the upstream bug that there's not a lock for the catalogs,
    i.e. records of a track can be inserted by web UI and CLI one after another.
    A workaround is to stop using `inotify` to update catalogs and let user specify a
    crontab job, making sure the catalogs update won't happen both in webUI and CLI.

--- run.sh
+++ run.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 
+example_conf_file=/var/www/html/config/ampache.cfg.php.dist
+
 # Update the .dist file in the volume, so that we *KNOW* that it is up-to-date
 # with the ampache version
-sudo -u www-data cp /ampache.cfg.php.dist /var/www/html/config/ampache.cfg.php.dist
+sudo -u www-data cp /ampache.cfg.php.dist ${example_conf_file}
 
-# Start a background process to auto-update the library
-(
-    # By default, use inotifywait to watch the library
-    while true; do
-        inotifywait -r /media &>/dev/null
-        # If inotifywait returns 1 then the inotify limit is too small
-        if [ "$?" == "1" ]; then
-            break
-        fi
-        sudo -u www-data php /var/www/html/bin/catalog_update.inc -a
-        sleep 30
-    done
-    # This is the fallback method when the number of inotify watches is too small.
-    # It simply auto-updates the library once every 24 hours
-    watchesNo="$(cat /proc/sys/fs/inotify/max_user_watches)"
-    echo "********"
-    echo "NOTICE:"
-    echo "The maximum number of inotify watches ($watchesNo) is too small for your"
-    echo "media collection. If you want to auto-update the library on changes, you must"
-    echo "increase the permitted number of inotify watches by writing to"
-    echo "/proc/sys/fs/inotify/max_user_watches or setting the sysctl setting"
-    echo "fs.inotify.max_user_watches and then restart the container."
-    echo ""
-    echo "Falling back to running an update approximately every 24 hours."
-    echo "********"
-    while true; do
-        sleep 24h
-        sudo -u www-data php /var/www/html/bin/catalog_update.inc -a
-    done
-) &
+# Crontab job to update catalogs
+# ATTENTION: duplicated tracks can be added when the catalogs are updated by both the web UI and CLI
+# Such updates can insert two records of a same track one after another by two different users: you(e.g. "admin") for web and "admin" for CLI
+# A lock for catalogs updating in Ampache should solve this problem
+# Here I choose to let the user specify the crontab rule of updating to make sure the catalogs are first updated by web UI once
+# and then by CLI periodically so that the race condition won't happen
+[ -n "${CRON_JOB_UPDATE_CATALOGS}" ] && (crontab -l 2>/dev/null; echo "${CRON_JOB_UPDATE_CATALOGS} php /var/www/html/bin/catalog_update.inc") | sort | uniq | crontab -
+service cron start
+service cron reload
+
 
 # run this in the foreground so Docker won't exit
 exec /usr/local/bin/apache2-foreground
